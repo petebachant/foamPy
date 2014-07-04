6a4466901e32f943cea8caf6556822b399b422e2
Pete Bachant <petebachant@gmail.com>
2014-07-04 11:46:07 -0400

    Rearranging things a bit

---
+++ dictionaries.py
@@ -0,0 +1,34 @@
+
+"""
+by Pete Bachant (c) 2014
+
+This module contains some useful classes and scripts for working with
+OpenFOAM. I'm not sure how it compares with PyFoam.
+
+"""
+from __future__ import division, print_function
+
+    
+def build_header(dictobject, version="2.3.x", fileclass="dictionary"):
+    """Creates the header for an OpenFOAM dictionary. Inputs are the 
+    object and version."""
+    return \
+    r"""/*--------------------------------*- C++ -*----------------------------------*\
+| =========                 |                                                 |
+| \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox           |
+|  \\    /   O peration     | Version:  %s                                 |
+|   \\  /    A nd           | Web:      www.OpenFOAM.org                      |
+|    \\/     M anipulation  |                                                 |
+\*---------------------------------------------------------------------------*/
+FoamFile
+{
+    version     2.0;
+    format      ascii;
+    class       %s;
+    object      %s;
+}
+""" % (version, fileclass, dictobject)
+
+
+if __name__ == "__main__":
+    print(build_header("controlDict", fileclass="volVectorField"))

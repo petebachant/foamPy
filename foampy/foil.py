a0fcd424aac9bbfa91afb74fccbeeabb9e6c8b7c
Pete Bachant <petebachant@gmail.com>
2015-05-05 17:32:14 -0400

    Start line and end line arguments now work

--- foampy/foil.py
+++ foampy/foil.py
@@ -2,6 +2,7 @@
 Module for working with foil data.
 """
 from __future__ import division, print_function
+import os
 
 class FoilData(object):
     """
@@ -20,18 +21,23 @@ class FoilData(object):
         """
         in_block = False
         with open(fpath, "r") as f:
-            for line in f.readlines():
-                try:
-                    a = [float(n) for n in line.replace(",", " ").split()]
-                    self.alpha.append(a[0])
-                    self.cl.append(a[1])
-                    self.cd.append(a[2])
-                    if len(a) > 3:
-                        self.cm.append(a[3])
-                    in_block = True
-                except (ValueError, IndexError):
-                    if in_block:
-                        break
+            for n, line in enumerate(f.readlines()):
+                if startline is not None and n+1 < startline:
+                    pass
+                elif endline is not None and n+1 > endline:
+                    pass
+                else:
+                    try:
+                        a = [float(n) for n in line.replace(",", " ").split()]
+                        self.alpha.append(a[0])
+                        self.cl.append(a[1])
+                        self.cd.append(a[2])
+                        if len(a) > 3:
+                            self.cm.append(a[3])
+                        in_block = True
+                    except (ValueError, IndexError):
+                        if in_block:
+                            break
 
     def write(self, fpath):
         """
@@ -58,5 +64,17 @@ def test_reformat_foildata():
     """
     ifp = "test/NACA_0021.dat"
     ofp = "test/NACA_0021.txt"
+    ofp2 = "test/NACA_0021_2.txt"
+    ofp3 = "test/NACA_0021_3.txt"
+    if os.path.isfile(ofp):
+        os.remove(ofp)
     reformat_foildata(ifp, ofp)
+    if os.path.isfile(ofp2):
+        os.remove(ofp2)
+    reformat_foildata(ifp, ofp2, startline=118)
+    if os.path.isfile(ofp3):
+        os.remove(ofp3)
+    reformat_foildata(ifp, ofp3, startline=118, endline=119)
+    
+    
     

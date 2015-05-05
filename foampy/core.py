466d9efee18cf58bb224c6e3ea14809f4030e02d
Pete Bachant <petebachant@gmail.com>
2015-05-05 16:41:51 -0400

    Update docstring in core module

--- foampy/core.py
+++ foampy/core.py
@@ -1,10 +1,6 @@
-
 """
-by Pete Bachant (c) 2014
-
 This module contains some useful classes and scripts for working with
-OpenFOAM. I'm not sure how it compares with PyFoam.
-
+OpenFOAM.
 """
 from __future__ import division, print_function
 import numpy as np

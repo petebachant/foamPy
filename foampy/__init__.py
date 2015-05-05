32fb77dc14b42f860a8b2b251ec03a1062999b0d
Pete Bachant <petebachant@gmail.com>
2015-05-05 16:40:11 -0400

    Start foil module

--- foampy/__init__.py
+++ foampy/__init__.py
@@ -2,3 +2,4 @@
 from . import core
 from .core import *
 from . import dictionaries
+from . import foil
---
+++ test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+This script runs tests on foamPy
+"""
+
+import foampy
+
+if __name__ == "__main__":
+    foampy.foil.test_reformat_foildata()

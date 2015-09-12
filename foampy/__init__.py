f3d63366c78418bce37353eb260df5111830390a
Pete Bachant <petebachant@gmail.com>
2015-09-12 19:12:30 -0400

    Attempt to detect OpenFOAM version in __init__.py

--- foampy/__init__.py
+++ foampy/__init__.py
@@ -1,6 +1,16 @@
 __version__ = "0.0.2"
 
+import os
+
+# Attempt to detect OpenFOAM version
+try:
+    wm_project_dir = os.environ["WM_PROJECT_DIR"]
+    foam_version = os.path.split(wm_project_dir)[-1].replace("OpenFOAM-", "")
+except KeyError:
+    foam_version = "x.x.x"
+
 from . import core
 from .core import *
 from . import dictionaries
 from . import foil
+from . import types

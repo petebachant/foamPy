2abbbf494953fa0536a533da0c71a7bda15b8b14
Pete Bachant <petebachant@gmail.com>
2015-09-12 19:12:49 -0400

    Change test script

--- test.py
+++ test.py
@@ -6,4 +6,6 @@ This script runs tests on foamPy
 import foampy
 
 if __name__ == "__main__":
-    foampy.foil.test_reformat_foildata()
+    
+#    foampy.foil.test_reformat_foildata()
+    foampy.types.test_foamdict()

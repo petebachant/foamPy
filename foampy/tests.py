a27b03a89af6442dc8e1be3d310a8fc046a98ed4
Pete Bachant <petebachant@gmail.com>
2016-07-13 19:27:53 -0400

    Add test for loading all torque and drag data

--- foampy/tests.py
+++ foampy/tests.py
@@ -1,8 +1,12 @@
-"""
-Tests for foamPy.
-"""
+"""Tests for foamPy."""
 
 from .core import *
 from .dictionaries import *
 from .types import *
 from .foil import *
+
+
+def test_load_all_torque_drag():
+    """Test the `load_all_torque_drag` function."""
+    t, torque, drag = load_all_torque_drag(casedir="test")
+    assert t.max() == 4.0
